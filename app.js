87727b26ff7ab491625a83bf05094dfb01e775ee
Anagha Chaudhari <143149376+anagha-chaudhari@users.noreply.github.com>
2024-12-05 22:49:19 +0530

    first

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+// set up socket io
+
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// ejs
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname,"public")));
+
+io.on("connection", function(socket){
+    console.log("connected");
+});
+
+app.get("/", function (req, res) {
+    res.render("index")
+})
+
+server.listen(3000)
