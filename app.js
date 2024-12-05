13ab33ff174ca510ac9647d7e119a5c47682e771
Anagha Chaudhari <143149376+anagha-chaudhari@users.noreply.github.com>
2024-12-05 23:28:54 +0530

    update

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const app = express();
-const path = require("path");
-
-// set up socket io
-
 const http = require("http");
+const path = require("path");
 const socketio = require("socket.io");
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-// ejs
-
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname,"public")));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function(socket){
-    console.log("connected");
+    console.log("User connected with ID: " + socket.id);
+
+    socket.on("send-location", function(data){
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnect", socket.id);
+    });
 });
 
-app.get("/", function (req, res) {
-    res.render("index")
-})
+app.get("/", function (req, res){
+    console.log("Rendering index");
+    res.render("index");
+});
 
-server.listen(3000)
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
